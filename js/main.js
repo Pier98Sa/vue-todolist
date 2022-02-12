68f096ebda4aa41b1d56e888607a5bd9f74c4045
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-02-12 18:17:43 +0100

    aggiunta validazione tramite booleano

--- js/main.js
+++ js/main.js
@@ -1,11 +1,33 @@
-const vue = new vue (
+const vue = new Vue (
     {
         el: '#root',
         data: {
+            newToDo: '',
+            ToDo: [
+                {
+                    text: 'Fare Colazione',
+                    done: true
+                },
+
+                {
+                    text: 'Consultare la documentazione',
+                    done: true
+                },
+
+                {
+                    text: 'Programmare',
+                    done: true
+                },
+
+                {
+                    text: 'Pranzare',
+                    done: false
+                }
+            ],
 
         },
         methods:{
-            
+
         }
     }
 );
