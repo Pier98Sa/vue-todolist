337db6fdce1ef37c7d91fa12b8b34f36ea6e5ac3
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-02-12 19:15:28 +0100

    aggiunta funzione per rimuovere gli elementi della lista

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ const vue = new Vue (
         methods:{
             //funzione per aggiungere un object tramite l'input
             addToDo() {
-                
+
                 if(this.newToDo == ''){
                     alert('Non hai inserito nessun nuovo to do')
                 }else{
@@ -37,6 +37,12 @@ const vue = new Vue (
                 }
                 
                 this.newToDo = '';
+            },
+
+            //funzione per rimuovere il to do dalla lista
+
+            removeToDo(index) {
+               this.ToDo[index].done = false;
             }
 
         }
